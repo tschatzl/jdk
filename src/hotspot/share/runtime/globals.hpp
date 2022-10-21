6b35685dd28b99b9e25713533c349e3dde00775a
J. Protopopovs <jprotopopov1122@gmail.com>
2022-10-21 19:43:26 +0200

    Add product flag to control G1 RemSet update strategy

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -1982,6 +1982,12 @@ const int ObjectAlignmentInBytes = 8;
           false AARCH64_ONLY(DEBUG_ONLY(||true)),                           \
              "Mark all threads after a safepoint, and clear on a modify "   \
              "fence. Add cleanliness checks.")                              \
+                                                                            \
+  develop(bool, TraceOptimizedUpcallStubs, false,                           \
+                "Trace optimized upcall stub generation")                   \
+                                                                            \
+  product(bool, G1TpRemsetInvestigationDirectUpdate, false,                 \
+                "Enable direct remset updates during GC pause")             \
 
 // end of RUNTIME_FLAGS
 
