03e8fef534a67c9ef1b02a095dd31365532d5d14
J. Protopopovs <jprotopopov1122@gmail.com>
2022-11-04 08:01:22 +0100

    Dirty G1 young regions after allocation

--- src/hotspot/share/gc/g1/g1CardTable.cpp
+++ src/hotspot/share/gc/g1/g1CardTable.cpp
@@ -37,7 +37,11 @@ void G1CardTable::g1_mark_as_young(const MemRegion& mr) {
 
 #ifndef PRODUCT
 void G1CardTable::verify_g1_young_region(MemRegion mr) {
+#ifdef DISABLE_TP_REMSET_INVESTIGATION
   verify_region(mr, g1_young_gen,  true);
+#else
+  verify_region(mr, G1CardTable::dirty_card_val(),  true);
+#endif
 }
 #endif
 
